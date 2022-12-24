c36e184a1baf303820f8eac42e11dcf57672aa07
17TheWord <17theword@gmail.com>
2022-12-24 12:53:53 +0800

    添加依赖httpx

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name="nonebot-plugin-warframe",  # 项目名称，保证它的唯一性，不要跟已存在的包名冲突即可
-    version="0.0.1",  # 程序版本
+    version="0.0.4",  # 程序版本
     author="17TheWord",  # 项目作者
     author_email="17theword@gmail.com",  # 作者邮件
     description="基于NoneBot的星际战甲事件查询插件",  # 项目的一句话描述
@@ -22,5 +22,6 @@ setuptools.setup(
         'nonebot2>=2.0.0b4',
         'nonebot-adapter-onebot>=2.1.1',
         'nonebot-plugin-guild-patch>=0.2.0',
+        'httpx>=0.23.1',
     ],
 )
