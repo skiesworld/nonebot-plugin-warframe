838fe505b18d4a1456f62d72d160ce41f7943a04
17TheWord <54731914+17TheWord@users.noreply.github.com>
2023-06-19 00:48:49 +0800

    Update __init__.py
    添加插件信息

--- nonebot_plugin_warframe/__init__.py
+++ nonebot_plugin_warframe/__init__.py
@@ -1,8 +1,17 @@
 from nonebot import on_command
+from nonebot.plugin import PluginMetadata
 from nonebot.internal.adapter import Message
 from nonebot.params import CommandArg
 
 from .data_source import chose_way, get_alerts
+
+__plugin_meta__ = PluginMetadata(
+    name="星际战甲事件查询",
+    description="通过奥迪斯网站API查询星际战甲国服事件",
+    usage="wf菜单",
+    type="application",
+    homepage="https://github.com/17TheWord/nonebot-plugin-warframe"
+)
 
 warframe = on_command("warframe", aliases={"星际战甲", "wf"}, priority=5)
 
